bf6059a8d899015dcd9fdb57bd2878507e69d35e
Konrad Kołakowski <vashpan@gmail.com>
2018-09-14 23:53:26 +0200

    Added initial iOS port of ISBench

---
+++ src/platform/ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ISBench
+//
+//  Created by Konrad Kołakowski on 14.09.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/platform/ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ISBench
+//
+//  Created by Konrad Kołakowski on 14.09.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
